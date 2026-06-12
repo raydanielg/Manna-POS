2ced268acb306a268cd8e88304f154413bc8886f
raydanielg <airezra2+1@gmail.com>
2026-06-12 13:23:43 +0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,62 @@ Route::get('/', function () {
     return view('landing');
 });
 
+// Product pages
+Route::get('/features', function () {
+    return view('product.features');
+});
+Route::get('/pricing', function () {
+    return view('product.pricing');
+});
+Route::get('/integrations', function () {
+    return view('product.integrations');
+});
+Route::get('/updates', function () {
+    return view('product.updates');
+});
+
+// Company pages
+Route::get('/about', function () {
+    return view('company.about');
+});
+Route::get('/contact', function () {
+    return view('company.contact');
+});
+Route::get('/careers', function () {
+    return view('company.careers');
+});
+Route::get('/blog', function () {
+    return view('company.blog');
+});
+
+// Support pages
+Route::get('/help', function () {
+    return view('support.help');
+});
+Route::get('/documentation', function () {
+    return view('support.documentation');
+});
+Route::get('/api', function () {
+    return view('support.api');
+});
+Route::get('/status', function () {
+    return view('support.status');
+});
+
+// Legal pages
+Route::get('/privacy', function () {
+    return view('legal.privacy');
+});
+Route::get('/terms', function () {
+    return view('legal.terms');
+});
+Route::get('/cookies', function () {
+    return view('legal.cookies');
+});
+Route::get('/gdpr', function () {
+    return view('legal.gdpr');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
