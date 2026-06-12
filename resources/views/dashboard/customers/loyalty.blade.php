bb42ac140bb7eb5da87d8dd17963f2ac62e97f9c
raydanielg <airezra2+1@gmail.com>
2026-06-12 14:02:26 +0300

    commit

--- resources/views/dashboard/customers/loyalty.blade.php
+++ resources/views/dashboard/customers/loyalty.blade.php
@@ -48,11 +48,11 @@
             Dashboard
         </a>
         <div class="nav-group-label">Customers</div>
-        <a href="#" class="nav-item">
+        <a href="{{ route('dashboard.customers.all') }}" class="nav-item">
             <svg fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/></svg>
             All Customers
         </a>
-        <a href="#" class="nav-item active">
+        <a href="{{ route('dashboard.customers.loyalty') }}" class="nav-item active">
             <svg fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"/></svg>
             Loyalty Program
         </a>
