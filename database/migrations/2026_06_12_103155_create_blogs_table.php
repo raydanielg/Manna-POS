ef65ba4bc6ad16267ed9450ba6978e3a7213ae06
raydanielg <airezra2+1@gmail.com>
2026-06-12 13:32:48 +0300

    commit

--- database/migrations/2026_06_12_103155_create_blogs_table.php
+++ database/migrations/2026_06_12_103155_create_blogs_table.php
@@ -15,6 +15,19 @@ class CreateBlogsTable extends Migration
     {
         Schema::create('blogs', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->string('category')->default('Article');
+            $table->text('excerpt');
+            $table->longText('content');
+            $table->string('cover_image');
+            $table->string('author_name');
+            $table->string('author_avatar')->nullable();
+            $table->string('author_title')->nullable();
+            $table->unsignedSmallInteger('read_time')->default(5);
+            $table->unsignedInteger('views')->default(0);
+            $table->boolean('is_published')->default(true);
+            $table->timestamp('published_at')->nullable();
             $table->timestamps();
         });
     }
