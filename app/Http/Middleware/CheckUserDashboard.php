44cd0fc8f35f28ef4c1ffcd52f2faa41924499c1
raydanielg <airezra2+1@gmail.com>
2026-06-16 14:52:27 +0300

    commit

--- app/Http/Middleware/CheckUserDashboard.php
+++ app/Http/Middleware/CheckUserDashboard.php
@@ -7,15 +7,11 @@ use Illuminate\Http\Request;
 
 class CheckUserDashboard
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
-     */
     public function handle(Request $request, Closure $next)
     {
+        if ($request->user()->role === 'admin') {
+            return redirect('/admin');
+        }
         return $next($request);
     }
 }
