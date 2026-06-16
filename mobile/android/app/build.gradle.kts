66e508e202f0ff262b0a95bb9ba222b9d4c857f8
raydanielg <airezra2+1@gmail.com>
2026-06-17 02:19:58 +0300

    commit

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -15,14 +15,12 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.mobile"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "com.mannapos.mobile"
         minSdk = 24
         targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
     buildTypes {
