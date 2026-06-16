66e5407044a16b2338bf0ea418d5cda88a718593
raydanielg <airezra2+1@gmail.com>
2026-06-16 16:08:15 +0300

    commit

--- check_seeder.php
+++
@@ -1,8 +0,0 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
-$app = require __DIR__ . '/bootstrap/app.php';
-$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-$c = App\Models\SystemConfig::count();
-$b = App\Models\BusinessCategory::count();
-$g = App\Models\PaymentGateway::count();
-echo "SystemConfigs: $c, BusinessCategories: $b, PaymentGateways: $g\n";
