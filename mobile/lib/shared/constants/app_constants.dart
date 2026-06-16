c68ce2d83c45d0f153744e7c752faf4a50426af4
raydanielg <airezra2+1@gmail.com>
2026-06-17 02:16:25 +0300

    commit

--- mobile/lib/shared/constants/app_constants.dart
+++ mobile/lib/shared/constants/app_constants.dart
@@ -1,6 +1,6 @@
 ﻿class AppConstants {
   // Change to your server IP for real device, or keep 10.0.2.2 for Android emulator
-  static const String baseUrl = 'http://192.168.1.136:8081/api';
+  static const String baseUrl = 'http://mannapos.co.tz/api';
   static const String tokenKey = 'manna_token';
   static const String userKey  = 'manna_user';
   static const String currency  = 'TSh';
