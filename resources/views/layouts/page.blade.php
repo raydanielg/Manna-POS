ad856e0dd8a4e7aa331b39946a809dcee1575b96
raydanielg <airezra2+1@gmail.com>
2026-06-19 00:22:32 +0300

    commit

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -6,6 +6,9 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', config('app.name', 'MannaPOS'))</title>
     <link rel="icon" type="image/png" href="{{ asset('icons8-dynamics-365-100.png') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
         tailwind.config = {
