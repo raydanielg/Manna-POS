05de19a69c5cf99a13f6e06279c84e40b9e57bcf
raydanielg <airezra2+1@gmail.com>
2026-06-19 12:40:55 +0300

    commit

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -3,20 +3,47 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Services\NextSmsService;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
 
 class ForgotPasswordController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller is responsible for handling password reset emails and
-    | includes a trait which assists in sending these notifications from
-    | your application to your users. Feel free to explore this trait.
-    |
-    */
-
     use SendsPasswordResetEmails;
+
+    protected NextSmsService $sms;
+
+    public function __construct(NextSmsService $sms)
+    {
+        $this->sms = $sms;
+    }
+
+    /**
+     * Send a reset link to the given user, and also SMS.
+     */
+    public function sendResetLinkEmail(Request $request)
+    {
+        $request->validate(['email' => 'required|email']);
+
+        $broker = $this->broker();
+        $user = $broker->getUser(['email' => $request->email]);
+
+        if ($user) {
+            $token = $broker->createToken($user);
+            $resetUrl = url(route('password.reset', ['token' => $token, 'email' => $user->email], false));
+
+            if ($user->phone) {
+                $this->sms->sendPasswordReset($user->phone, $resetUrl);
+            }
+        }
+
+        $response = $broker->sendResetLink(
+            $request->only('email')
+        );
+
+        return $response == Password::RESET_LINK_SENT
+            ? back()->with('status', __($response))
+            : back()->withErrors(['email' => __($response)]);
+    }
 }
