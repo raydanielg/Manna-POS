f2fc45b60a4ae3bd6fc78322f0ceb740a8c6916d
raydanielg <airezra2+1@gmail.com>
2026-06-19 13:12:00 +0300

    commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            CountrySeeder::class,
+            CurrencySeeder::class,
             AdminSeeder::class,
             DemoSeeder::class,
             AdminDemoSeeder::class,
